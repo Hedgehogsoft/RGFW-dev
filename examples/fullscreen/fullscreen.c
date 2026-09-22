ce20ecb19ead0eaa6bd1cb098a4c2f638af5b614
ColleagueRiley <ColleagueRiley@gmail.com>
2026-09-22 12:19:24 -0400

    use opengl

--- examples/fullscreen/fullscreen.c
+++ examples/fullscreen/fullscreen.c
@@ -1,11 +1,20 @@
+#define RGFW_OPENGL
 #define RGFW_DEBUG
 #define RGFW_IMPLEMENTATION
 #include "RGFW.h"
 
-int main(void) {
-	RGFW_init("RGFW Example", 0);
+#define RGL_LOAD_IMPLEMENTATION
+#include "../gl33/rglLoad.h"
 
-    RGFW_window* win = RGFW_createWindow("RGFW flags", 200, 200, 600, 400, RGFW_windowAllowDND);
+int main(void) {
+	RGFW_init("RGFW Example", RGFW_initOpenGL);
+
+	if (RGL_loadGL3((RGLloadfunc)RGFW_getProcAddress_OpenGL)) {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+	RGFW_window* win = RGFW_createWindow("RGFW flags", 200, 200, 600, 400, RGFW_windowAllowDND | RGFW_windowOpenGL);
     RGFW_window_setExitKey(win, RGFW_keyEscape);
 
     while (RGFW_window_shouldClose(win) == RGFW_FALSE) {
@@ -18,7 +27,12 @@ int main(void) {
         } else if (RGFW_isKeyPressed(RGFW_keyQ)) {
             RGFW_window_setFullscreen(win, RGFW_fullscreenNone);
         }
-    }
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		RGFW_window_swapBuffers_OpenGL(win);
+	}
 
     RGFW_window_close(win);
     RGFW_deinit();
